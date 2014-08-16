0537ac99d2aa47dd9ba6b9e41b3b707fdf5af60c
joshj5hawk <joshj5hawk@gmail.com>
2014-08-15 23:18:13 -0500

    Crafting and Passive Spawning Done
    Signed-off-by: joshj5hawk <joshj5hawk@gmail.com>

--- main/java/com/joshj5hawk/crafting/SummoningRecipes.java
+++ main/java/com/joshj5hawk/crafting/SummoningRecipes.java
@@ -3,8 +3,10 @@ package com.joshj5hawk.crafting;
 import com.joshj5hawk.handler.ConfigurationFile;
 import com.joshj5hawk.main.SummoningTable;
 
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 
 public class SummoningRecipes
@@ -23,17 +25,56 @@ public class SummoningRecipes
 	{
 		if(ConfigurationFile.easyMode == true)
 		{
-			if(item == Items.iron_ingot && item2 == Items.leather || item == Items.leather && item2 == Items.diamond)
-				return new ItemStack(SummoningTable.itemSummoningBookCow.setMaxDamage(63), 1);
-		
+			if(item == Items.diamond && item2 == Items.leather || item == Items.leather && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookCow);
+			if(item == Items.diamond && item2 == Items.porkchop || item == Items.porkchop && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookPig);
+			if(item == Items.diamond && item2 == ItemBlock.getItemFromBlock(Blocks.wool) || item == ItemBlock.getItemFromBlock(Blocks.wool) && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookSheep);
+			if(item == Items.diamond && item2 == Items.chicken || item == Items.chicken && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookChicken);
+			if(item == Items.diamond && item2 == Items.mushroom_stew || item == Items.mushroom_stew && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookMooshroom);
+			if(item == Items.diamond && item2 == Items.emerald || item == Items.emerald && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookVillager);
+			if(item == Items.diamond && item2 == Items.snowball || item == Items.snowball && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookSnowGolem);
+			if(item == Items.diamond && item2 == Items.apple || item == Items.apple && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookBat);
+			if(item == Items.diamond && item2 == Items.saddle || item == Items.saddle && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookHorse);
+			if(item == Items.diamond && item2 == Items.fish || item == Items.fish && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookOcelot);
+			if(item == Items.diamond && item2 == Items.dye || item == Items.emerald && item2 == Items.diamond)
+				return new ItemStack(SummoningTable.itemSummoningBookSquid);
+			
+			
 			return null;
 		}
 		else
 		{
-			if(item == Items.diamond && item2 == Items.leather || item == Items.leather && item2 == Items.iron_ingot)
-			{
-				return new ItemStack(SummoningTable.itemSummoningBookCow.setMaxDamage(5), 1);
-			}
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.leather || item == Items.leather && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookCow);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.porkchop || item == Items.porkchop && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookPig);
+			if(item == SummoningTable.itemSummoningCore && item2 == ItemBlock.getItemFromBlock(Blocks.wool) || item == ItemBlock.getItemFromBlock(Blocks.wool) && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookSheep);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.chicken || item == Items.chicken && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookChicken);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.mushroom_stew || item == Items.mushroom_stew && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookMooshroom);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.emerald || item == Items.emerald && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookVillager);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.snowball || item == Items.snowball && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookSnowGolem);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.apple || item == Items.apple && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookBat);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.saddle || item == Items.saddle && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookHorse);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.fish || item == Items.fish && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookOcelot);
+			if(item == SummoningTable.itemSummoningCore && item2 == Items.dye || item == Items.emerald && item2 == SummoningTable.itemSummoningCore)
+				return new ItemStack(SummoningTable.itemSummoningBookSquid);
 			
 			return null;
 		}
--- main/java/com/joshj5hawk/recipies/STCraftingRecipies.java
+++ main/java/com/joshj5hawk/recipies/STCraftingRecipies.java
@@ -2,9 +2,9 @@ package com.joshj5hawk.recipies;
 
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.oredict.OreDictionary;
+
+import com.joshj5hawk.main.SummoningTable;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -16,10 +16,18 @@ public class STCraftingRecipies
 		shapedRecipies();
 		shapelessRecipies();
 	}
-	
+	//GameRegistry.addRecipe(new ItemStack(RESULT), new Object[] {"CCC", "CCC", "CCC", 'C', VALUE OF C});
 	public static void shapedRecipies()
 	{
-		
+		GameRegistry.addRecipe(new ItemStack(SummoningTable.itemSummoningCore), new Object[] {"IDI", "DBD", "IDI", 'I', Items.iron_ingot, 'D', Items.diamond, 'B', Blocks.diamond_block});
+		GameRegistry.addRecipe(new ItemStack(SummoningTable.itemPassiveCraftingOrb), new Object[] {"GPG", "SDC", "GBG", 'G', Blocks.glass, 'P', Items.porkchop, 'S', Items.mushroom_stew, 'C', Items.chicken, 'B', Items.beef, 'D', Items.diamond});
+		GameRegistry.addRecipe(new ItemStack(SummoningTable.itemPassiveCraftingOrb), new Object[] {"GBG", "SDC", "GPG", 'G', Blocks.glass, 'P', Items.porkchop, 'S', Items.mushroom_stew, 'C', Items.chicken, 'B', Items.beef, 'D', Items.diamond});
+		GameRegistry.addRecipe(new ItemStack(SummoningTable.itemNeutralCraftingOrb), new Object[] {"GIG", "EDL", "GAG", 'G', Blocks.glass, 'I', Blocks.iron_block, 'E', Items.ender_pearl, 'D', Items.diamond, 'L', Items.lead, 'A', Items.gold_ingot});
+		GameRegistry.addRecipe(new ItemStack(SummoningTable.itemNeutralCraftingOrb), new Object[] {"GAG", "EDL", "GIG", 'G', Blocks.glass, 'I', Blocks.iron_block, 'E', Items.ender_pearl, 'D', Items.diamond, 'L', Items.lead, 'A', Items.gold_ingot});
+		GameRegistry.addRecipe(new ItemStack(SummoningTable.itemHostileCraftingOrb), new Object[] {"GFG", "BDE", "GSG", 'G', Blocks.glass, 'F', Items.rotten_flesh, 'B', Items.bone, 'E', Items.spider_eye, 'S', Items.gunpowder, 'D', Items.diamond});
+		GameRegistry.addRecipe(new ItemStack(SummoningTable.itemHostileCraftingOrb), new Object[] {"GSG", "BDE", "GFG", 'G', Blocks.glass, 'F', Items.rotten_flesh, 'B', Items.bone, 'E', Items.spider_eye, 'S', Items.gunpowder, 'D', Items.diamond});
+		GameRegistry.addRecipe(new ItemStack(SummoningTable.blockSummoningTableIdle), new Object[] {"CCC", "PSH", "WNW", 'C', (new ItemStack(Blocks.carpet, 1, 14)), 'P', SummoningTable.itemPassiveCraftingOrb, 'S', SummoningTable.itemSummoningCore, 'H', SummoningTable.itemHostileCraftingOrb, 'W', Blocks.planks, 'N', SummoningTable.itemNeutralCraftingOrb});
+
 	}
 	
 	public static void shapelessRecipies()
